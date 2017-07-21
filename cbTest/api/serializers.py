dfd7a7c5b29ec05315621842473e0c00de1e4757
Pablo Iocco <pablo_iocco@hotmail.com>
2017-07-21 11:33:30 -0400

    Serializer explanation

--- cbTest/api/serializers.py
+++ cbTest/api/serializers.py
@@ -5,12 +5,13 @@ Created on 20-07-2017
 '''
 from rest_framework import serializers
 from .models import Address
+#Regular Serializer
 class AddressSerializer(serializers.HyperlinkedModelSerializer):
-    #url = serializers.HyperlinkedIdentityField(view_name='address_list')
     class Meta:
         model = Address
         fields = ('id', 'address', 'latitude', 'longitude', 'elevation')
-        
+
+#Serializer used to hyperlink an address
 class AddressLinkSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name='addres_detail')
     class Meta:
