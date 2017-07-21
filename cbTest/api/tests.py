dac844f5596b1186c0688325bdbd7befb9546941
Pablo Iocco <pablo_iocco@hotmail.com>
2017-07-21 11:27:46 -0400

    Unit testing

--- cbTest/api/tests.py
+++ cbTest/api/tests.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+import requests
 
 # Create your tests here.
+class UrlTestCase(TestCase):
+    baseUrl = "localhost:8000/"
+    def createUrl(self):
+        postData = {"address='Libertad 835'"}
+
+        create = requests.post(baseUrl+'api/',params = postData)
+        self.assertEqual(create.status_code, 201)
+    def getAddresses(self):
+        r = requests.get(baseUrl+'api/')
+        self.assertEqual(r.status_code, 200)
+        
